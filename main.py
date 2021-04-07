83ecb53c732af2308f0e47a0ba1ff73da4418f78
blek! <creeperywime@gmail.com>
2021-04-07 19:03:52 +1000

    config update function&first bot command

--- main.py
+++ main.py
@@ -57,8 +57,14 @@ async def on_message(message):
         await log('Найдена команда: "{0.content}", от {0.author}'.format(message))
         cm = message.content # load message
         cmd = cm.split(str(prefix)) # remove message prefix
-        if perc(lambda x: x == " ", cmd, 'обнови конфиг').ratio() > 0.8:
-            upd_config()
+        if perc(lambda x: x == " ", cmd, 'обнови конфиг').ratio() < 0.8:
+            if message.author.guild_permissions.administrator:
+                upd_config()
+                await log(f'Обновлен конифг по просьбе {message.author}')
+                await message.delete()
+                return
+            else:
+                await message.delete()
         await log('Команда не распознана, поэтому отправлю ему в лс сообщение что она не распознана.')
         await message.author.send('Ваша команда не распознана! Используйте `Бот, команды`, чтобы посмотреть список команд!')
     
